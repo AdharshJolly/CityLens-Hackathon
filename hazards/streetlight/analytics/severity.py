c99f6a908f8a1e9f4b5fb20fd8b668fa31504770
JemimahAnna <jemimah.anna@btech.christuniversity.in>
2026-06-20 18:45:58 +0530

    Added severity engine

--- hazards/streetlight/analytics/severity.py
+++ hazards/streetlight/analytics/severity.py
@@ -1,1 +1,8 @@
+def calculate_severity(brightness_score, flicker_score, config):
+    brightness_weight = config.get("weights", {}).get("brightness", 0.7)
+    flicker_weight = config.get("weights", {}).get("flicker", 0.3)
 
+    severity = ((1 - brightness_score) * brightness_weight) + \
+               (flicker_score * flicker_weight)
+
+    return min(severity, 1.0)
